b49d910dfc6809cdee8b8138bee5d39dec509f2d
Nathan Kelderman <nathanjkelderman@hotmail.com>
2017-10-03 15:37:25 -0400

    Created simple test.c program totest functions

---
+++ test.c
@@ -0,0 +1,12 @@
+#include "functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char* removeDuplicates(char word[]);
+
+int main(int argc, char** argv) {
+	char word[10] = {a, b, c, c};
+	char* final = removeDuplicates(word);
+	printf("%s", final);
+	return 0;
+}
